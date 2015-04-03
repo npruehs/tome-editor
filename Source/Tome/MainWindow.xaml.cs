21b30f62ce4bcdfe8163031f3faf974ff8df6831
Nick Prühs <dev@npruehs.de>
2015-04-03 20:05:51 +0200

    CHANGED: Project - Code cleanup of initial files.

--- Source/Tome/MainWindow.xaml.cs
+++ Source/Tome/MainWindow.xaml.cs
@@ -1,28 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Tome">
+//   Copyright (c) Tome. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace Tome
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    using System.Windows;
+
     public partial class MainWindow : Window
     {
+        #region Constructors and Destructors
+
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
+
+        #endregion
     }
 }
