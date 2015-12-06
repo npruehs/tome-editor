d96639ba5794610ed4be824810e6acffc534eb74
Nick Prühs <dev@npruehs.de>
2015-12-06 22:45:11 +0100

    ADDED: Documentation - Missing comment.

--- Source/Tome/Util/pathutils.h
+++ Source/Tome/Util/pathutils.h
@@ -6,6 +6,12 @@
 
 namespace Tome
 {
+    /**
+     * @brief combinePaths Combines the paths specified by the two passes strings.
+     * @param first First path to combine.
+     * @param second Second path to combine.
+     * @return Both paths, combined with the platform-specific path seperator.
+     */
     QString combinePaths(const QString& first, const QString& second)
     {
         return QDir::cleanPath(first + QDir::separator() + second);
