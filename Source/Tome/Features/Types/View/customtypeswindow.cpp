5450f7819317304c86ccace8363f2efb5d00242e
Nick Prühs <dev@npruehs.de>
2016-10-06 21:27:43 +0200

    FIXED: Types - Adding a new list type no longer causes the application to crash.

--- Source/Tome/Features/Types/View/customtypeswindow.cpp
+++ Source/Tome/Features/Types/View/customtypeswindow.cpp
@@ -104,7 +104,7 @@ void CustomTypesWindow::on_actionNew_List_triggered()
                 this->typesController.addList(
                     this->listWindow->getListName(),
                     this->listWindow->getListItemType(),
-                    this->enumerationWindow->getTypeSetName());
+                    this->listWindow->getTypeSetName());
 
         // Update view.
         this->ui->tableWidget->insertRow(0);
