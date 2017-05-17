97f479ea498d415602bb9ef60592f219ace267ab
Nick Prühs <dev@npruehs.de>
2017-05-17 22:56:19 +0200

    ADDED: Documentation - Help classes.

--- Source/Tome/Features/Help/View/aboutwindow.h
+++ Source/Tome/Features/Help/View/aboutwindow.h
@@ -7,11 +7,18 @@ namespace Ui {
     class AboutWindow;
 }
 
+/**
+ * @brief Window for showing copyright and license information about Tome.
+ */
 class AboutWindow : public QDialog
 {
         Q_OBJECT
 
     public:
+        /**
+         * @brief Constructs a new window for showing copyright and license information about Tome.
+         * @param parent Optional owner widget.
+         */
         explicit AboutWindow(QWidget *parent = 0);
         ~AboutWindow();
 
